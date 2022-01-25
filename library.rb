f43d422eadcc228ee8f27779e17ad76fff476542
KARANGWA Ntwali Josue <kntwalijos@gmail.com>
2022-01-25 18:38:00 +0200

    Ädd accessors for person class

--- library.rb
+++ library.rb
@@ -1,46 +1,23 @@
 class Person
   # constructor with @age, @id, @name instance variables
-  def initialize(name="unknown", age, parent_mission=true)
+  def initialize(name="unknown", age, parent_permission=true)
     @id = Random.rand(1..100)
     @name = name
     @age = age
   end
-  # getter for @id
-  def id
-    @id
-  end
-  # getter for @name
-  def name
-    @name
-  end
-  # getter for @age
-  def age
-    @age
-  end
+  # add accessor for getting @id
+  attr_reader :id
+  
+  # accessor for get and set methods for @age and @name
+  attr_accessor :name
+  attr_accessor :age
 
+  private # set is_of_age to private method
   def is_of_age?
     @age >= 18? true: false
   end
-  # set method for @name
-  def name=(name)
-    @name=name
-  end
-
-  # set method for @age
-  def age=(age)
-    @age=age
-  end
 
   def can_use_services?
-    if (is_of_age? === true)
-      return true
-    else
-      return false
-    end
+    (is_of_age? || parent_permission) true : false
   end
-end
-
-class Student
-  
-  
 end
