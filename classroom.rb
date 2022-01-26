2f38410a084f7c63948b59682a552e8ef09fd46b
KARANGWA Ntwali Josue <kntwalijos@gmail.com>
2022-01-26 21:09:10 +0200

    Add student method

--- classroom.rb
+++ classroom.rb
@@ -2,9 +2,14 @@ require_relative './student'
 
 class Classroom
   attr_accessor :label
+  attr_reader :student
 
   def initialize(label)
     @label = label
+    @student = []
   end
 
+  def add_student(student)
+    @student.push(student)
+  end
 end
