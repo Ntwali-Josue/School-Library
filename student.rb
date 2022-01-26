b17d98f4314ae540c1370c1cf3166fdb0ebe3499
KARANGWA Ntwali Josue <kntwalijos@gmail.com>
2022-01-26 22:38:09 +0200

    Use reader attribute for classroom instance variable

--- student.rb
+++ student.rb
@@ -1,7 +1,7 @@
 require_relative './person'
 
 class Student < Person
-  attr_accessor :classroom
+  attr_reader :classroom
 
   def initialize(age, classroom, parent_permission: true, name: 'unknown')
     super(name, age, parent_permission)
