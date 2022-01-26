dd0bc3ee187ddf5f7f8b7d770b1c1330dabfb4f3
KARANGWA Ntwali Josue <kntwalijos@gmail.com>
2022-01-26 22:36:22 +0200

    Add rents relationship

--- book.rb
+++ book.rb
@@ -1,8 +1,12 @@
+require_relative './rental'
+
 class Book
+  attr_reader :rents
   attr_accessor :title, :author
 
   def initialize(title, author)
     @title = title
     @author = author
+    @rents = []
   end
 end
