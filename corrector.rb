d742bf75a0cfd5dfacaf412b20c6d9548e722602
KARANGWA Ntwali Josue <kntwalijos@gmail.com>
2022-01-26 13:09:21 +0200

    Remove if condition

--- corrector.rb
+++ corrector.rb
@@ -1,10 +1,6 @@
 class Corrector
-  def initialize(name)
-    @name = name
-  end
-
   def correct_name(name)
-    name.capitalize
-    name.slice(0, 10) if name.length > 10
+    name.capitalize!
+    name.slice(0, 10)
   end
 end
