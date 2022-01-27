b8fc4556185d37d8991c3f5a0da7dac4cf812b71
KARANGWA Ntwali Josue <kntwalijos@gmail.com>
2022-01-27 19:43:32 +0200

    Edit parameter for initialize function in student class

--- student.rb
+++ student.rb
@@ -3,8 +3,8 @@ require_relative './person'
 class Student < Person
   attr_reader :classroom
 
-  def initialize(age, classroom, parent_permission: true, name: 'unknown')
-    super(name, age, parent_permission)
+  def initialize(age:, classroom:, parent_permission: true, name: 'unknown')
+    super(name: name, age: age, parent_permission: parent_permission)
     @classroom = classroom
   end
 
